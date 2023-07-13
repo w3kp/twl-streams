a786da9095b366f573816c3b83491b7088cfa1a1
Paul B <pebartocillo@apper.ph>
2023-07-13 21:23:43 +0800

    chore: added transcribing html

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+const _ = require("lodash");
 const WebSocket = require("ws");
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server, clientTracking: true });
+const path = require("path");
+
+const bodyParser = require("body-parser");
+
+//Include Google Speech to Text
+const speech = require("@google-cloud/speech");
+const speechClient = new speech.SpeechClient();
+
+//Configure Transcription Request
+const transcriptionConfig = {
+    config: {
+        encoding: "MULAW",
+        sampleRateHertz: 8000,
+        languageCode: "en-GB",
+    },
+    interimResults: true, // If you want interim results, set this to true
+};
+
+let activeCalls = [];
 
 wss.on("connection", (ws) => {
     console.log("New connection initiated!");
+
+    let recognizeStream = null;
 
     ws.on("message", (message) => {
         const msg = JSON.parse(message);
@@ -15,12 +37,70 @@ wss.on("connection", (ws) => {
                 break;
             case "start":
                 console.log(`Starting media stream ${msg.streamSid}`);
+
+                ws.streamSid = msg.streamSid;
+
+                // Create Stream to the Google Speech to Text API
+                recognizeStream = speechClient
+                .streamingRecognize(transcriptionConfig)
+                .on("error", console.error)
+                .on("data", (data) => {
+                    wss.clients.forEach((client) => {
+                    if (
+                        client.readyState === WebSocket.OPEN &&
+                        client.subscribedStream === msg.streamSid
+                    ) {
+                        client.send(
+                        JSON.stringify({
+                            stream: msg.streamSid,
+                            event: "interim-transcription",
+                            text: data.results[0].alternatives[0].transcript,
+                        })
+                        );
+                    }
+                    });
+                });
+
+                activeCalls.push({
+                    twilioStreamSid: msg.streamSid,
+                    fromNumber: msg.start.customParameters.number,
+                });
+
+                wss.clients.forEach((client) => {
+                    client.send(
+                        JSON.stringify({
+                        event: "updateCalls",
+                        activeCalls,
+                        })
+                    );
+                });
+
+                console.log(`There are ${activeCalls.length} active calls`);
                 break;
             case "media":
-                // console.log(`Receiving audio...`);
+                // Write Media Packets to the recognize stream
+                recognizeStream.write(msg.media.payload);
                 break;
-            case "connected":
-                console.log(`Call has ended.`);
+            case "stop":
+                console.log(`Call Has Ended`);
+                console.log(ws.streamSid);
+                const i = activeCalls.findIndex(
+                    (stream) => stream.streamSid === ws.streamSid
+                );
+                activeCalls.splice(i, 1);
+                wss.clients.forEach((client) => {
+                    client.send(
+                    JSON.stringify({
+                        event: "updateCalls",
+                        activeCalls: activeCalls,
+                    })
+                    );
+                });
+                recognizeStream.destroy();
+                break;
+            case "subscribe":
+                console.log("Client Subscribed");
+                ws.subscribedStream = msg.streamSid;
                 break;
             default:
                 break;
@@ -28,21 +108,25 @@ wss.on("connection", (ws) => {
     })
 });
 // <Stream url="wss://${req.headers.host}/" />
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello CHATMSG!"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.post("/", (req, res) => {
     res.set("Content-Type", "text/xml");
-
+    console.log(req.body.From);
     res.send(`
         <Response>
-            <Start>
-                <Stream url="wss://${req.headers.host}/" />
-            </Start>
-            <Say>Hi Don Chicken! Welcome to Chat MSG! Ipapapatwag ka sa Senado! Humanda ka!</Say>
-            <Pause length="60" />
+        <Start>
+            <Stream url="wss://${req.headers.host}/">
+            <Parameter name="number" value="${req.body.From}"/>
+            </Stream>
+        </Start>
+        <Say>I will stream the next 60 seconds of audio through your websocket</Say>
+        <Pause length="60" />
         </Response>
-    `)
+    `);
 })
 
 console.log("Listening at port 8080...");

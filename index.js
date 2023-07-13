a55d3d7df3029561e82060c0175f79128ed4d77e
Paul B <pebartocillo@apper.ph>
2023-07-13 14:26:02 +0800

    init: first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const WebSocket = require("ws");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+    console.log("New connection initiated!");
+
+    ws.on("message", (message) => {
+        const msg = JSON.parse(message);
+        switch (msg.event) {
+            case "connected":
+                console.log(`A new call has connected.`);
+                break;
+            case "start":
+                console.log(`Starting media stream ${msg.streamSid}`);
+                break;
+            case "media":
+                console.log(`Receiving audio...`);
+                break;
+            case "connected":
+                console.log(`Call has ended.`);
+                break;
+            default:
+                break;
+        }
+    })
+});
+// <Stream url="wss://${req.headers.host}/" />
+
+app.get("/", (req, res) => res.send("Hello CHATMSG!"));
+
+app.post("/", (req, res) => {
+    res.set("Content-Type", "text/xml");
+
+    res.send(`
+        <Response>
+            <Start>
+                <Stream url="wss:// /" />
+            </Start>
+            <Say>Hi Don! Welcome to Chat MSG! </Say>
+            <Pause length="60" />
+        </Response>
+    `)
+})
+
+console.log("Listening at port 8080...");
+server.listen(8080);
